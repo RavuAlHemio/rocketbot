6b097b5149fed52913a0aaefee8a053b49db996f
Ondřej Hošek <ondra.hosek@gmail.com>
2021-12-30 15:52:42 +0100

    text: document !distance and compact its output

---
+++ rocketbot_plugin_text/help/distance.md
@@ -0,0 +1,17 @@
+*Usage:* `{cpfx}distance STRING1 STRING2`
+
+Outputs information about how much `STRING1` and `STRING2` differ.
+
+If the strings contain spaces, standard command string tokenization rules apply: strings in quotation marks (`"`) are considered one argument, quotation marks can be escaped using backslashes (`\`), and backslashes can be doubled to escape them.
+
+The following metrics are calculated:
+
+* `<`, `==` or `>`: standard case-sensitive character-wise comparison
+* `L`: Levenshtein distance
+* `D-L`: Damerau-Levenshtein distance
+* `OSA`: Optimal String Alignment distance
+* `H`: Hamming distance (only works for strings of the same length; `!H` appears for strings with differing length)
+* `J`: Jaro similarity
+* `J-W`: Jaro-Winkler similarity
+* `S-D`: Sørensen-Dice similarity
+* `SCS`: both strings use the same set of characters (or `!SCS` if not)
--- rocketbot_plugin_text/src/lib.rs
+++ rocketbot_plugin_text/src/lib.rs
@@ -28,6 +28,61 @@ impl TextPlugin {
             "no"
         }
     }
+
+    async fn channel_command_distance(&self, channel_message: &ChannelMessage, command: &CommandInstance) {
+        let interface = match self.interface.upgrade() {
+            None => return,
+            Some(i) => i,
+        };
+
+        let string1 = &command.args[0];
+        let string2 = &command.args[1];
+
+        let comparison_str = match string1.cmp(string2) {
+            Ordering::Greater => ">",
+            Ordering::Equal => "==",
+            Ordering::Less => "<",
+        };
+
+        let lev = strsim::levenshtein(string1, string2);
+        let dam_lev = strsim::damerau_levenshtein(string1, string2);
+        let optimal_string_align_dist = strsim::osa_distance(string1, string2);
+
+        let hamming = strsim::hamming(string1, string2);
+        let hamming_string = match hamming {
+            Ok(dist) => format!("H={}", dist),
+            Err(e) => "!H".to_owned(),
+        };
+
+        let jaro = strsim::jaro(string1, string2);
+        let jaro_winkler = strsim::jaro_winkler(string1, string2);
+        let sorensen_dice = strsim::sorensen_dice(string1, string2);
+
+        let same_set_str = if Self::same_char_set(string1, string2) {
+            "SCS"
+        } else {
+            "!SCS"
+        };
+
+        let response = format!(
+            concat!(
+                "{:?} {} {:?}:",
+                "\nL={} D-L={} OSA={} {}",
+                "\nJ={:.3}% J-W={:.3}% S-D={:.3}%",
+                "\n{}",
+            ),
+            string1, comparison_str, string2,
+            lev, dam_lev, optimal_string_align_dist, hamming_string,
+            jaro * 100.0, jaro_winkler * 100.0, sorensen_dice * 100.0,
+            same_set_str,
+        );
+
+        send_channel_message!(
+            interface,
+            &channel_message.channel.name,
+            &response,
+        ).await;
+    }
 }
 #[async_trait]
 impl RocketBotPlugin for TextPlugin {
@@ -58,58 +113,16 @@ impl RocketBotPlugin for TextPlugin {
     }
 
     async fn channel_command(&self, channel_message: &ChannelMessage, command: &CommandInstance) {
-        let interface = match self.interface.upgrade() {
-            None => return,
-            Some(i) => i,
-        };
+        if command.name == "distance" {
+            self.channel_command_distance(channel_message, command).await
+        }
+    }
 
-        let string1 = &command.args[0];
-        let string2 = &command.args[1];
-
-        let comparison_str = match string1.cmp(string2) {
-            Ordering::Greater => ">",
-            Ordering::Equal => "==",
-            Ordering::Less => "<",
-        };
-
-        let lev = strsim::levenshtein(string1, string2);
-        let dam_lev = strsim::damerau_levenshtein(string1, string2);
-        let optimal_string_align_dist = strsim::osa_distance(string1, string2);
-
-        let hamming = strsim::hamming(string1, string2);
-        let hamming_string = match hamming {
-            Ok(dist) => dist.to_string(),
-            Err(e) => format!("({})", e),
-        };
-
-        let jaro = strsim::jaro(string1, string2);
-        let jaro_winkler = strsim::jaro_winkler(string1, string2);
-        let sorensen_dice = strsim::sorensen_dice(string1, string2);
-
-        let same_set_str = Self::yes_no(Self::same_char_set(string1, string2));
-
-        let response = format!(
-            concat!(
-                "{:?} {} {:?}:",
-                "\nLevenshtein distance: {}",
-                "\nDamerau-Levenshtein distance: {}",
-                "\nOptimal String Alignment distance: {}",
-                "\nHamming distance: {}",
-                "\nJaro similarity: {:.3}%",
-                "\nJaro-Winkler similarity: {:.3}%",
-                "\nS\u{F8}rensen-Dice similarity: {:.3}%",
-                "\nSame character set: {}",
-            ),
-            string1, comparison_str, string2,
-            lev, dam_lev, optimal_string_align_dist,
-            hamming_string, jaro * 100.0, jaro_winkler * 100.0, sorensen_dice * 100.0,
-            same_set_str,
-        );
-
-        send_channel_message!(
-            interface,
-            &channel_message.channel.name,
-            &response,
-        ).await;
+    async fn get_command_help(&self, command_name: &str) -> Option<String> {
+        if command_name == "distance" {
+            Some(include_str!("../help/distance.md").to_owned())
+        } else {
+            None
+        }
     }
 }
