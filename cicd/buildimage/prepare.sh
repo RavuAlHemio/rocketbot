3edbf0ef23c628bf17ab8841da92a22f463a0ad1
Ondřej Hošek <ondra.hosek@gmail.com>
2022-01-09 11:58:44 +0100

    CI/CD: write scripts to prepare a build image
    this should allow faster builds by pre-shipping all dependencies

---
+++ cicd/buildimage/prepare.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Prepare the build environment to speed up builds.
+#
+
+# update packages
+pacman -Syu --noconfirm
+
+# install required packages
+pacman -S --needed --noconfirm cargo git python-toml
+
+# check out the repository
+git clone https://github.com/RavuAlHemio/rocketbot.git /rocketbot
+cd /rocketbot
+
+# pre-build everything once
+cargo build --all-targets || exit 1
+cargo build --all-targets --release || exit 1
+
+# the current version of all dependencies should now be prepared
+
+# clean up the repository
+cd /
+rm -rf /rocketbot
