2ecde07ffcd9725030a48d2cc4b11639a3304857
Ondřej Hošek <ondra.hosek@gmail.com>
2024-03-25 00:09:22 +0100

    bim: !bimdivscore: cast sum to bigint

--- rocketbot_plugin_bim/src/lib.rs
+++ rocketbot_plugin_bim/src/lib.rs
@@ -3636,7 +3636,7 @@ impl BimPlugin {
                     )
                 SELECT
                     nnrn.rider_username,
-                    SUM(nnrn.line_number)
+                    CAST(SUM(nnrn.line_number) AS bigint) div_score
                 FROM not_null_ride_numbers nnrn
                 WHERE MOD(nnrn.vehicle_number, nnrn.line_number) = 0
                 {}
