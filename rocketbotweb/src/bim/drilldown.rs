b934ca7c66157a786d8c124a884fbbd3d0479586
Ondřej Hošek <ondra.hosek@gmail.com>
2024-11-27 11:46:15 +0100

    web bim drilldown: implement filtering

--- rocketbotweb/src/bim/drilldown.rs
+++ rocketbotweb/src/bim/drilldown.rs
@@ -17,11 +17,11 @@ use crate::templating::filters;
 
 
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-struct KnownColumn {
+struct GroupColumn {
     pub field_name: &'static str,
     pub column_heading: &'static str,
 }
-impl KnownColumn {
+impl GroupColumn {
     pub const fn new(
         field_name: &'static str,
         column_heading: &'static str,
@@ -29,6 +29,55 @@ impl KnownColumn {
         Self {
             field_name,
             column_heading,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct FilterColumn {
+    pub field_name: &'static str,
+    pub field_type: FilterFieldType,
+}
+impl FilterColumn {
+    pub const fn new(
+        field_name: &'static str,
+        field_type: FilterFieldType,
+    ) -> Self {
+        Self {
+            field_name,
+            field_type,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+enum FilterFieldType {
+    String,
+    U128,
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+enum FilterFieldValue {
+    String(String),
+    U128(u128),
+}
+impl FilterFieldValue {
+    pub fn to_sql_string(&self) -> String {
+        match self {
+            Self::String(s) => {
+                let mut ret = String::with_capacity(s.len() + 2);
+                ret.push('\'');
+                for c in s.chars() {
+                    if c == '\'' {
+                        // double up
+                        ret.push('\'');
+                    }
+                    ret.push(c);
+                }
+                ret.push('\'');
+                ret
+            },
+            Self::U128(u) => format!("{}", u),
         }
     }
 }
@@ -43,19 +92,38 @@ struct BimDrilldownTemplate {
 }
 
 
-static KNOWN_GROUP_TO_COLUMN_INFO: LazyLock<HashMap<&'static str, KnownColumn>> = LazyLock::new(|| {
+static KNOWN_GROUP_TO_COLUMN_INFO: LazyLock<HashMap<&'static str, GroupColumn>> = LazyLock::new(|| {
     let mut ret = HashMap::new();
-    ret.insert("company", KnownColumn::new("tbl.company", "company"));
-    ret.insert("rider", KnownColumn::new("tbl.rider_username", "rider"));
-    ret.insert("timestamp.year", KnownColumn::new("EXTRACT(YEAR FROM tbl.\"timestamp\")", "year"));
-    ret.insert("timestamp.month", KnownColumn::new("EXTRACT(MONTH FROM tbl.\"timestamp\")", "month"));
-    ret.insert("timestamp.day", KnownColumn::new("EXTRACT(DAY FROM tbl.\"timestamp\")", "day"));
-    ret.insert("timestamp.hour", KnownColumn::new("EXTRACT(HOUR FROM tbl.\"timestamp\")", "hour"));
-    ret.insert("timestamp.minute", KnownColumn::new("EXTRACT(MINUTE FROM tbl.\"timestamp\")", "minute"));
-    ret.insert("timestamp.second", KnownColumn::new("EXTRACT(SECOND FROM tbl.\"timestamp\")", "second"));
-    ret.insert("line", KnownColumn::new("tbl.line", "line"));
-    ret.insert("vehicle-type", KnownColumn::new("tbl.vehicle_type", "vehicle type"));
-    ret.insert("vehicle-number", KnownColumn::new("tbl.vehicle_number", "vehicle"));
+    ret.insert("company", GroupColumn::new("tbl.company", "company"));
+    ret.insert("rider", GroupColumn::new("tbl.rider_username", "rider"));
+    ret.insert("timestamp.year", GroupColumn::new("EXTRACT(YEAR FROM tbl.\"timestamp\")", "year"));
+    ret.insert("timestamp.month", GroupColumn::new("EXTRACT(MONTH FROM tbl.\"timestamp\")", "month"));
+    ret.insert("timestamp.day", GroupColumn::new("EXTRACT(DAY FROM tbl.\"timestamp\")", "day"));
+    ret.insert("timestamp.weekday", GroupColumn::new("EXTRACT(DOW FROM tbl.\"timestamp\")", "weekday"));
+    ret.insert("timestamp.hour", GroupColumn::new("EXTRACT(HOUR FROM tbl.\"timestamp\")", "hour"));
+    ret.insert("timestamp.minute", GroupColumn::new("EXTRACT(MINUTE FROM tbl.\"timestamp\")", "minute"));
+    ret.insert("timestamp.second", GroupColumn::new("EXTRACT(SECOND FROM tbl.\"timestamp\")", "second"));
+    ret.insert("line", GroupColumn::new("tbl.line", "line"));
+    ret.insert("vehicle-type", GroupColumn::new("tbl.vehicle_type", "vehicle type"));
+    ret.insert("vehicle-number", GroupColumn::new("tbl.vehicle_number", "vehicle"));
+    ret
+});
+
+static KNOWN_FILTER_TO_COLUMN_INFO: LazyLock<HashMap<&'static str, FilterColumn>> = LazyLock::new(|| {
+    use FilterFieldType as F;
+    let mut ret = HashMap::new();
+    ret.insert("company", FilterColumn::new("tbl.company", F::String));
+    ret.insert("rider", FilterColumn::new("tbl.rider_username", F::String));
+    ret.insert("timestamp.year", FilterColumn::new("EXTRACT(YEAR FROM tbl.\"timestamp\")", F::U128));
+    ret.insert("timestamp.month", FilterColumn::new("EXTRACT(MONTH FROM tbl.\"timestamp\")", F::U128));
+    ret.insert("timestamp.day", FilterColumn::new("EXTRACT(DAY FROM tbl.\"timestamp\")", F::U128));
+    ret.insert("timestamp.weekday", FilterColumn::new("EXTRACT(DOW FROM tbl.\"timestamp\")", F::U128));
+    ret.insert("timestamp.hour", FilterColumn::new("EXTRACT(HOUR FROM tbl.\"timestamp\")", F::U128));
+    ret.insert("timestamp.minute", FilterColumn::new("EXTRACT(MINUTE FROM tbl.\"timestamp\")", F::U128));
+    ret.insert("timestamp.second", FilterColumn::new("EXTRACT(SECOND FROM tbl.\"timestamp\")", F::U128));
+    ret.insert("line", FilterColumn::new("tbl.line", F::String));
+    ret.insert("vehicle-type", FilterColumn::new("tbl.vehicle_type", F::String));
+    ret.insert("vehicle-number", FilterColumn::new("tbl.vehicle_number", F::String));
     ret
 });
 
@@ -72,6 +140,8 @@ pub(crate) async fn handle_bim_drilldown(request: &Request<Incoming>) -> Result<
     let empty = Vec::with_capacity(0);
     let groups = query_pairs_multi.get("group")
         .unwrap_or(&empty);
+    let filter_strings = query_pairs_multi.get("filter")
+        .unwrap_or(&empty);
 
     let mut known_columns = Vec::with_capacity(groups.len());
     {
@@ -87,6 +157,38 @@ pub(crate) async fn handle_bim_drilldown(request: &Request<Incoming>) -> Result<
             if !is_fresh {
                 return return_400(&format!("duplicate grouping field {:?}", &*group), &query_pairs).await;
             }
+        }
+    }
+
+    let mut known_filters = Vec::with_capacity(filter_strings.len());
+    {
+        let mut seen_filters = HashSet::new();
+        for filter_string in filter_strings {
+            let (key, value) = match filter_string.split_once('=') {
+                Some(kv) => kv,
+                None => return return_400(&format!("filter string {:?} does not contain an equals sign", &*filter_string), &query_pairs).await,
+            };
+
+            let known_column = match KNOWN_FILTER_TO_COLUMN_INFO.get(key) {
+                Some(kc) => *kc,
+                None => return return_400(&format!("unknown filter field {:?}", key), &query_pairs).await,
+            };
+            let is_fresh = seen_filters.insert(key);
+            if !is_fresh {
+                return return_400(&format!("duplicate filter field {:?}", key), &query_pairs).await;
+            }
+
+            let field_value = match known_column.field_type {
+                FilterFieldType::String => FilterFieldValue::String(value.to_owned()),
+                FilterFieldType::U128 => {
+                    let parsed: u128 = match value.parse() {
+                        Ok(p) => p,
+                        Err(_) => return return_400(&format!("invalid numeric value {:?} for filter field {:?}", value, key), &query_pairs).await,
+                    };
+                    FilterFieldValue::U128(parsed)
+                },
+            };
+            known_filters.push((known_column, field_value));
         }
     }
 
@@ -115,16 +217,30 @@ pub(crate) async fn handle_bim_drilldown(request: &Request<Incoming>) -> Result<
     }
     select_sql_columns_string.push_str("CAST(COUNT(*) AS character varying(256)) entry_count");
 
+    let mut where_string = String::new();
+    for (column, value) in known_filters {
+        if where_string.len() > 0 {
+            where_string.push_str(" AND ");
+        } else {
+            where_string.push_str("WHERE ");
+        }
+        where_string.push_str(&column.field_name);
+        where_string.push_str(" = ");
+        where_string.push_str(&value.to_sql_string());
+    }
+
     let query = format!(
         "
             SELECT
                 {}
             FROM
                 bim.rides_and_ridden_vehicles tbl
+            {}
             {} {}
             {} {}
         ",
         select_sql_columns_string,
+        where_string,
         if sql_columns_string.len() > 0 { "GROUP BY" } else { "" }, sql_columns_string,
         if sql_columns_string.len() > 0 { "ORDER BY" } else { "" }, sql_columns_string,
     );
--- rocketbotweb/ts/src/bim/drilldown.ts
+++ rocketbotweb/ts/src/bim/drilldown.ts
@@ -1,7 +1,20 @@
 export namespace Drilldown {
-    function addSelectRow(form: HTMLElement, addParagraphElement: HTMLElement, options: string[], selectedOption: string|null) {
+    function addRemoveButton(parent: HTMLElement, elementToRemove?: HTMLElement): HTMLInputElement {
+        if (elementToRemove === undefined) {
+            elementToRemove = parent;
+        }
+
+        const removeButton = document.createElement("input");
+        removeButton.type = "button";
+        removeButton.value = "\u2212";
+        removeButton.addEventListener("click", () => elementToRemove.parentElement?.removeChild(elementToRemove));
+        parent.appendChild(removeButton);
+        return removeButton;
+    }
+
+    function addGroupRow(groupingDivElement: HTMLElement, addParagraphElement: HTMLElement, options: string[], selectedOption: string|null) {
         const paragraphElement = document.createElement("p");
-        form.insertBefore(paragraphElement, addParagraphElement);
+        groupingDivElement.insertBefore(paragraphElement, addParagraphElement);
 
         const selectElement = document.createElement("select");
         selectElement.name = "group";
@@ -16,11 +29,95 @@ export namespace Drilldown {
             selectElement.appendChild(optionElement);
         }
 
-        const removeButton = document.createElement("input");
-        removeButton.type = "button";
-        removeButton.value = "\u2212";
-        removeButton.addEventListener("click", () => paragraphElement.parentElement?.removeChild(paragraphElement));
-        paragraphElement.appendChild(removeButton);
+        addRemoveButton(paragraphElement);
+    }
+
+    function addFilterRow(filteringDivElement: HTMLElement, addParagraphElement: HTMLElement, options: string[], selectedPair: [string, string]|null) {
+        const paragraphElement = document.createElement("p");
+        paragraphElement.classList.add("filter-row");
+        filteringDivElement.insertBefore(paragraphElement, addParagraphElement);
+
+        const selectElement = document.createElement("select");
+        selectElement.classList.add("filter-key");
+        paragraphElement.appendChild(selectElement);
+
+        paragraphElement.appendChild(document.createTextNode(" = "));
+
+        const textElement = document.createElement("input");
+        textElement.type = "text";
+        textElement.classList.add("filter-value");
+        paragraphElement.appendChild(textElement);
+
+        for (const option of options) {
+            const optionElement = document.createElement("option");
+            optionElement.innerText = option;
+            if (selectedPair !== null && option === selectedPair[0]) {
+                optionElement.selected = true;
+            }
+            selectElement.appendChild(optionElement);
+        }
+
+        if (selectedPair !== null) {
+            textElement.value = selectedPair[1];
+        }
+
+        addRemoveButton(paragraphElement);
+    }
+
+    function makeDataDiv(form: HTMLElement, className: string): HTMLDivElement {
+        const divElement = document.createElement("div");
+        divElement.classList.add(className);
+        form.appendChild(divElement);
+        return divElement;
+    }
+
+    function makeAddButton(dataDiv: HTMLElement, label: string): [HTMLParagraphElement, HTMLInputElement] {
+        const addParagraph = document.createElement("p");
+        dataDiv.appendChild(addParagraph);
+
+        const addButton = document.createElement("input");
+        addButton.type = "button";
+        addButton.value = label;
+        addParagraph.appendChild(addButton);
+
+        return [addParagraph, addButton];
+    }
+
+    function modifyFormSubmission(form: HTMLFormElement, event: SubmitEvent) {
+        // any filters?
+        const filterRows = Array.from(form.querySelectorAll(".filter-row"));
+        if (filterRows.length === 0) {
+            // no; just do the default thing
+            return;
+        }
+
+        // construct key-value pairs
+        const filters: string[] = [];
+        for (const filterRow of filterRows) {
+            const keySelect = <HTMLSelectElement|null>filterRow.querySelector("select.filter-key");
+            const valueInput = <HTMLInputElement|null>filterRow.querySelector("input.filter-value");
+
+            if (keySelect === null || valueInput === null) {
+                // ?!
+                return;
+            }
+
+            filters.push(`${keySelect.value}=${valueInput.value}`);
+        }
+
+        // replace filter rows with inputs
+        for (const filterRow of filterRows) {
+            filterRow.parentElement?.removeChild(filterRow);
+        }
+        for (const filter of filters) {
+            const filterElement = document.createElement("input");
+            filterElement.type = "hidden";
+            filterElement.name = "filter";
+            filterElement.value = filter;
+            form.appendChild(filterElement);
+        }
+
+        // the new fields are submitted, so no preventDefault
     }
 
     function doSetUp(options: string[]) {
@@ -31,11 +128,20 @@ export namespace Drilldown {
 
         const searchString = window.location.search;
         const groupByColumns: string[] = [];
+        const filterColumns: [string, string][] = [];
         if (searchString.startsWith("?")) {
             const searchPairs = new URLSearchParams(searchString.substring(1));
             for (const [searchKey, searchValue] of searchPairs) {
                 if (searchKey === "group") {
                     groupByColumns.push(searchValue);
+                } else if (searchKey === "filter") {
+                    const equalsIndex = searchValue.indexOf("=");
+                    if (equalsIndex === -1) {
+                        continue;
+                    }
+                    const filterKey = searchValue.substring(0, equalsIndex);
+                    const filterValue = searchValue.substring(equalsIndex + 1);
+                    filterColumns.push([filterKey, filterValue]);
                 }
             }
         }
@@ -44,25 +150,31 @@ export namespace Drilldown {
         form.method = "get";
         drilldownControls.appendChild(form);
 
-        const addParagraphElement = document.createElement("p");
-        form.appendChild(addParagraphElement);
+        const filteringDivElement = makeDataDiv(form, "filters");
+        const groupingDivElement = makeDataDiv(form, "groups");
 
+        const [addFilterParagraphElement, addFilterButton] = makeAddButton(filteringDivElement, "+filter");
+        addFilterButton.addEventListener("click", () => addFilterRow(filteringDivElement, addFilterParagraphElement, options, null));
+
+        const [addGroupParagraphElement, addGroupButton] = makeAddButton(groupingDivElement, "+group");
+        addGroupButton.addEventListener("click", () => addGroupRow(groupingDivElement, addGroupParagraphElement, options, null));
+
+        for (const [filterKey, filterValue] of filterColumns) {
+            addFilterRow(filteringDivElement, addFilterParagraphElement, options, [filterKey, filterValue]);
+        }
         for (const groupByColumn of groupByColumns) {
-            addSelectRow(form, addParagraphElement, options, groupByColumn);
+            addGroupRow(groupingDivElement, addGroupParagraphElement, options, groupByColumn);
         }
 
-        const addButton = document.createElement("input");
-        addButton.type = "button";
-        addButton.value = "+";
-        addButton.addEventListener("click", () => addSelectRow(form, addParagraphElement, options, null));
-        addParagraphElement.appendChild(addButton);
-
-        addParagraphElement.appendChild(document.createTextNode(" "));
+        const submitParagraphElement = document.createElement("p");
+        form.appendChild(submitParagraphElement);
 
         const submitButton = document.createElement("input");
         submitButton.type = "submit";
         submitButton.value = "pivot";
-        addParagraphElement.appendChild(submitButton);
+        submitParagraphElement.appendChild(submitButton);
+
+        form.addEventListener("submit", event => modifyFormSubmission(form, event));
     }
 
     export function setUp(options: string[]) {
