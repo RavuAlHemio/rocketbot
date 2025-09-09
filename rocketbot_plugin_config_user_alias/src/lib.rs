e1dd6e3e455f6a3c6d0b7cc927f3e3773f695c8e
Ondřej Hošek <ondrej.hosek@tuwien.ac.at>
2025-09-09 13:44:01 +0200

    config_user_alias: config keys are now regexes

--- rocketbot_plugin_config_user_alias/src/lib.rs
+++ rocketbot_plugin_config_user_alias/src/lib.rs
@@ -1,17 +1,20 @@
-use std::collections::HashMap;
+use std::borrow::Cow;
+use std::collections::BTreeMap;
 use std::sync::Weak;
 
 use async_trait::async_trait;
+use regex::Regex;
 use rocketbot_interface::JsonValueExtensions;
 use rocketbot_interface::interfaces::{RocketBotInterface, RocketBotPlugin};
 use rocketbot_interface::sync::RwLock;
+use rocketbot_string::regex::EnjoyableRegex;
 use serde_json;
 use tracing::error;
 
 
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Config {
-    lowercase_alias_to_username: HashMap<String, String>,
+    alias_regex_to_username: BTreeMap<EnjoyableRegex, String>,
 }
 
 
@@ -19,20 +22,24 @@ pub struct ConfigUserAliasPlugin {
     config: RwLock<Config>,
 }
 impl ConfigUserAliasPlugin {
-    fn try_get_config(config: serde_json::Value) -> Result<Config, &'static str> {
-        let mut lowercase_alias_to_username = HashMap::new();
-        let entries = config["lowercase_alias_to_username"]
-            .entries().ok_or("lowercase_alias_to_username not an object")?;
+    fn try_get_config(config: serde_json::Value) -> Result<Config, Cow<'static, str>> {
+        let mut alias_regex_to_username = BTreeMap::new();
+        let entries = config["alias_regex_to_username"]
+            .entries().ok_or("alias_regex_to_username not an object")?;
         for (key, val) in entries {
-            lowercase_alias_to_username.insert(
-                key.to_owned(),
-                val
-                    .as_str().ok_or("lowercase_alias_to_username item is not a string")?
-                    .to_owned(),
+            let key_regex = Regex::new(key)
+                .map_err(|e| Cow::Owned(format!("regex {:?} is invalid: {}", key, e)))?;
+            let key_enjoyable_regex = EnjoyableRegex::from_regex(key_regex);
+            let value_string = val
+                .as_str().ok_or("alias_regex_to_username item is not a string")?
+                .to_owned();
+            alias_regex_to_username.insert(
+                key_enjoyable_regex,
+                value_string,
             );
         }
         Ok(Config {
-            lowercase_alias_to_username,
+            alias_regex_to_username,
         })
     }
 }
@@ -56,9 +63,13 @@ impl RocketBotPlugin for ConfigUserAliasPlugin {
     }
 
     async fn username_resolution(&self, username: &str) -> Option<String> {
-        let alias_lower = username.to_lowercase();
         let config_guard = self.config.read().await;
-        config_guard.lowercase_alias_to_username.get(&alias_lower).map(|un| un.clone())
+        for (regex, actual_username) in &config_guard.alias_regex_to_username {
+            if regex.is_match(&username) {
+                return Some(actual_username.clone());
+            }
+        }
+        None
     }
 
     async fn configuration_updated(&self, new_config: serde_json::Value) -> bool {
