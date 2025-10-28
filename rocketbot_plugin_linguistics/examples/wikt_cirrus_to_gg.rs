aa22265c51f171dffa8975f9b23b835ece7e720e
Ondřej Hošek <ondra.hosek@gmail.com>
2025-10-28 11:34:55 +0100

    linguistics: add German gender loader
    uses the cirrussearch exports from Wiktionary, since those contain the
    necessary metadata

---
+++ rocketbot_plugin_linguistics/examples/wikt_cirrus_to_gg.rs
@@ -0,0 +1,102 @@
+//! Wiktionary `cirrussearch` export to German gender loader
+//!
+//! 1. Obtain a current `enwiktionary-...-cirrussearch-content.json.gz` from
+//! https://dumps.wikimedia.org/other/cirrussearch/ or a mirror.
+//!
+//! 2. Create a TOML configuration file:
+//! ```toml
+//! cirrus_json_gz_path = "..."
+//! db_conn_string = "..."
+//! ```
+//!
+//! 3. Run this tool, passing the path to the TOML configuration file.
+
+
+use std::collections::BTreeSet;
+use std::env::args_os;
+use std::ffi::OsString;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+use std::process::ExitCode;
+
+use flate2::read::GzDecoder;
+use serde::{Deserialize, Serialize};
+use tokio_postgres::NoTls;
+use toml;
+
+
+#[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+struct Config {
+    pub cirrus_json_gz_path: PathBuf,
+    pub db_conn_string: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+struct Page {
+    pub title: String,
+    #[serde(rename = "category")] pub categories: BTreeSet<String>,
+}
+
+#[tokio::main]
+async fn main() -> ExitCode {
+    let args: Vec<OsString> = args_os().collect();
+    if args.len() == 2 && args[1].to_string_lossy().starts_with("-") {
+        eprintln!("Usage: {} [CONFIG.TOML]", args[0].display());
+        return ExitCode::FAILURE;
+    }
+    let config_path = if args.len() > 1 {
+        PathBuf::from(&args[1])
+    } else {
+        PathBuf::from("config.toml")
+    };
+
+    let config_string = std::fs::read_to_string(&config_path)
+        .expect("failed to read config file");
+    let config: Config = toml::from_str(&config_string)
+        .expect("failed to parse config file");
+
+    let (mut conn, internal_conn) = tokio_postgres::connect(&config.db_conn_string, NoTls)
+        .await.expect("failed to connect to database");
+    tokio::spawn(async move {
+        if let Err(e) = internal_conn.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    let gz_file = File::open(&config.cirrus_json_gz_path)
+        .expect("failed to open gz file");
+    let cirrus_file = GzDecoder::new(gz_file);
+    let mut cirrus_buffy = BufReader::new(cirrus_file);
+
+    let xact = conn.transaction()
+        .await.expect("failed to start database transaction");
+    let insert_stmt = xact.prepare("INSERT INTO linguistics.german_genders (word, masculine, feminine, neuter) VALUES ($1, $2, $3, $4)")
+        .await.expect("failed to prepare insertion statement");
+
+    let mut buf = Vec::new();
+    loop {
+        buf.clear();
+        cirrus_buffy.read_until(b'\n', &mut buf)
+            .expect("reading failed");
+        if buf.len() == 0 {
+            // EOF
+            break;
+        }
+
+        let Ok(page): Result<Page, _> = serde_json::from_slice(&buf)
+            else { continue };
+        if !page.categories.contains("German nouns") {
+            continue;
+        }
+        xact.execute(&insert_stmt, &[
+            &page.title,
+            &page.categories.contains("German masculine nouns"),
+            &page.categories.contains("German feminine nouns"),
+            &page.categories.contains("German neuter nouns"),
+        ])
+            .await.expect("failed to insert database row");
+    }
+    xact.commit().await.expect("committing transaction failed");
+    ExitCode::SUCCESS
+}
