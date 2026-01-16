0c87b69307504de62c2b0ea3bf19947193871901
Ondřej Hošek <ondrej.hosek@tuwien.ac.at>
2026-01-16 12:52:25 +0100

    linguistics: Wikimedia Cirrus dumps are now multi-file and bzip2

--- rocketbot_plugin_linguistics/examples/wikt_cirrus_to_gg.rs
+++ rocketbot_plugin_linguistics/examples/wikt_cirrus_to_gg.rs
@@ -20,7 +20,7 @@ use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use std::process::ExitCode;
 
-use flate2::read::GzDecoder;
+use bzip2_rs::decoder::DecoderReader;
 use rocketbot_plugin_linguistics::GenderFlags;
 use serde::{Deserialize, Serialize};
 use tokio_postgres::NoTls;
@@ -29,7 +29,7 @@ use toml;
 
 #[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 struct Config {
-    pub cirrus_json_gz_path: PathBuf,
+    pub cirrus_json_bz2_paths: Vec<PathBuf>,
     pub db_conn_string: String,
     #[serde(default)] pub empty_first: bool,
 }
@@ -66,13 +66,9 @@ async fn main() -> ExitCode {
         }
     });
 
-    let gz_file = File::open(&config.cirrus_json_gz_path)
-        .expect("failed to open gz file");
-    let cirrus_file = GzDecoder::new(gz_file);
-    let mut cirrus_buffy = BufReader::new(cirrus_file);
-
     let xact = conn.transaction()
         .await.expect("failed to start database transaction");
+
     let insert_stmt = xact.prepare(
         "
             INSERT INTO linguistics.german_genders
@@ -90,61 +86,71 @@ async fn main() -> ExitCode {
             .await.expect("failed to execute deletion statement");
     }
 
-    let mut buf = Vec::new();
-    loop {
-        buf.clear();
-        cirrus_buffy.read_until(b'\n', &mut buf)
-            .expect("reading failed");
-        if buf.len() == 0 {
-            // EOF
-            break;
-        }
+    for bz2_path in &config.cirrus_json_bz2_paths {
+        eprintln!("processing {}", bz2_path.display());
 
-        let Ok(page): Result<Page, _> = serde_json::from_slice(&buf)
-            else { continue };
-        if
-                !page.categories.contains("German nouns")
-                && !page.categories.contains("German proper nouns")
-                && !page.categories.contains("German singularia tantum")
-                && !page.categories.contains("German pluralia tantum")
-                && !page.categories.contains("German masculine nouns")
-                && !page.categories.contains("German feminine nouns")
-                && !page.categories.contains("German neuter nouns") {
-            continue;
-        }
+        let bz2_file = File::open(bz2_path)
+            .expect("failed to open bz2 file");
+        let cirrus_file = DecoderReader::new(bz2_file);
+        let mut cirrus_buffy = BufReader::new(cirrus_file);
 
-        let mut gender_flags = GenderFlags::empty();
-        if page.categories.contains("German masculine nouns") {
-            gender_flags |= GenderFlags::MASCULINE;
-        }
-        if page.categories.contains("German feminine nouns") {
-            gender_flags |= GenderFlags::FEMININE;
-        }
-        if page.categories.contains("German neuter nouns") {
-            gender_flags |= GenderFlags::NEUTER;
-        }
-        if page.categories.contains("German singularia tantum") {
-            gender_flags |= GenderFlags::SINGULARE_TANTUM;
-        }
-        if page.categories.contains("German pluralia tantum") {
-            gender_flags |= GenderFlags::PLURALE_TANTUM;
-        }
-        if page.categories.contains("German male given names") {
-            gender_flags |= GenderFlags::MALE_GIVEN;
-        }
-        if page.categories.contains("German female given names") {
-            gender_flags |= GenderFlags::FEMALE_GIVEN;
-        }
-        if page.categories.contains("German unisex given names") {
-            gender_flags |= GenderFlags::UNISEX_GIVEN;
-        }
+        let mut buf = Vec::new();
+        loop {
+            buf.clear();
+            cirrus_buffy.read_until(b'\n', &mut buf)
+                .expect("reading failed");
+            if buf.len() == 0 {
+                // EOF
+                break;
+            }
 
-        xact.execute(&insert_stmt, &[
-            &page.title,
-            &gender_flags.bits(),
-        ])
-            .await.expect("failed to insert database row");
+            let Ok(page): Result<Page, _> = serde_json::from_slice(&buf)
+                else { continue };
+            if
+                    !page.categories.contains("German nouns")
+                    && !page.categories.contains("German proper nouns")
+                    && !page.categories.contains("German singularia tantum")
+                    && !page.categories.contains("German pluralia tantum")
+                    && !page.categories.contains("German masculine nouns")
+                    && !page.categories.contains("German feminine nouns")
+                    && !page.categories.contains("German neuter nouns") {
+                continue;
+            }
+
+            let mut gender_flags = GenderFlags::empty();
+            if page.categories.contains("German masculine nouns") {
+                gender_flags |= GenderFlags::MASCULINE;
+            }
+            if page.categories.contains("German feminine nouns") {
+                gender_flags |= GenderFlags::FEMININE;
+            }
+            if page.categories.contains("German neuter nouns") {
+                gender_flags |= GenderFlags::NEUTER;
+            }
+            if page.categories.contains("German singularia tantum") {
+                gender_flags |= GenderFlags::SINGULARE_TANTUM;
+            }
+            if page.categories.contains("German pluralia tantum") {
+                gender_flags |= GenderFlags::PLURALE_TANTUM;
+            }
+            if page.categories.contains("German male given names") {
+                gender_flags |= GenderFlags::MALE_GIVEN;
+            }
+            if page.categories.contains("German female given names") {
+                gender_flags |= GenderFlags::FEMALE_GIVEN;
+            }
+            if page.categories.contains("German unisex given names") {
+                gender_flags |= GenderFlags::UNISEX_GIVEN;
+            }
+
+            xact.execute(&insert_stmt, &[
+                &page.title,
+                &gender_flags.bits(),
+            ])
+                .await.expect("failed to insert database row");
+        }
     }
+
     xact.commit().await.expect("committing transaction failed");
     ExitCode::SUCCESS
 }
