f0984934fa585d313bcd09c85d6d1093ec2d0b75
Ondřej Hošek <ondra.hosek@gmail.com>
2026-03-01 04:14:02 +0100

    bim: move topriders into its own module

---
+++ rocketbot_plugin_bim/src/top.rs
@@ -0,0 +1,164 @@
+use std::collections::HashMap;
+
+use rocketbot_interface::send_channel_message;
+use rocketbot_interface::commands::CommandInstance;
+use rocketbot_interface::model::ChannelMessage;
+use tracing::error;
+
+use crate::{BimPlugin, connect_ride_db, LookbackRange};
+
+
+impl BimPlugin {
+    pub(crate) async fn channel_command_topriders(&self, channel_message: &ChannelMessage, command: &CommandInstance) {
+        let interface = match self.interface.upgrade() {
+            None => return,
+            Some(i) => i,
+        };
+
+        let config_guard = self.config.read().await;
+
+        let lookback_range: LookbackRange = match Self::lookback_range_from_command(command) {
+            Some(lr) => lr,
+            None => {
+                send_channel_message!(
+                    interface,
+                    &channel_message.channel.name,
+                    "Hey, no mixing options that mean different time ranges!",
+                ).await;
+                return;
+            },
+        };
+
+        let ride_conn = match connect_ride_db(&config_guard).await {
+            Ok(c) => c,
+            Err(_) => {
+                send_channel_message!(
+                    interface,
+                    &channel_message.channel.name,
+                    "Failed to open database connection. :disappointed:",
+                ).await;
+                return;
+            },
+        };
+
+        let ride_rows_res = Self::timestamp_query(
+            &ride_conn,
+            "
+                SELECT r.rider_username, CAST(COUNT(*) AS bigint) ride_count
+                FROM bim.rides r
+                {LOOKBACK_TIMESTAMP}
+                GROUP BY r.rider_username
+            ",
+            "WHERE r.\"timestamp\" >= $1",
+            "",
+            lookback_range,
+            &[],
+        ).await;
+        let ride_rows = match ride_rows_res {
+            Ok(r) => r,
+            Err(e) => {
+                error!("failed to query most active riders: {}", e);
+                send_channel_message!(
+                    interface,
+                    &channel_message.channel.name,
+                    "Failed to query database. :disappointed:",
+                ).await;
+                return;
+            },
+        };
+
+        let mut rider_to_ride_and_vehicle_count = HashMap::new();
+        for row in ride_rows {
+            let rider_username: String = row.get(0);
+            let ride_count: i64 = row.get(1);
+
+            let rider_ride_and_vehicle_count = rider_to_ride_and_vehicle_count
+                .entry(rider_username.clone())
+                .or_insert((0i64, 0i64));
+            rider_ride_and_vehicle_count.0 += ride_count;
+        }
+
+        let vehicle_rows_res = Self::timestamp_query(
+            &ride_conn,
+            "
+                SELECT i.rider_username, CAST(COUNT(*) AS bigint) vehicle_count
+                FROM (
+                    SELECT DISTINCT r.rider_username, r.company, rv.vehicle_number
+                    FROM bim.rides r
+                    INNER JOIN bim.ride_vehicles rv ON rv.ride_id = r.id
+                    WHERE rv.coupling_mode = 'R'
+                    {LOOKBACK_TIMESTAMP}
+                ) i
+                GROUP BY i.rider_username
+            ",
+            "AND r.\"timestamp\" >= $1",
+            "",
+            lookback_range,
+            &[],
+        ).await;
+        let vehicle_rows = match vehicle_rows_res {
+            Ok(r) => r,
+            Err(e) => {
+                error!("failed to query most active riders with vehicles: {}", e);
+                send_channel_message!(
+                    interface,
+                    &channel_message.channel.name,
+                    "Failed to query database. :disappointed:",
+                ).await;
+                return;
+            },
+        };
+
+        for row in vehicle_rows {
+            let rider_username: String = row.get(0);
+            let vehicle_count: i64 = row.get(1);
+
+            let rider_ride_and_vehicle_count = rider_to_ride_and_vehicle_count
+                .entry(rider_username.clone())
+                .or_insert((0i64, 0i64));
+            rider_ride_and_vehicle_count.1 += vehicle_count;
+        }
+
+        let mut rider_and_ride_and_vehicle_count: Vec<(String, i64, i64)> = rider_to_ride_and_vehicle_count
+            .iter()
+            .map(|(r, (rc, vc))| (r.clone(), *rc, *vc))
+            .collect();
+        rider_and_ride_and_vehicle_count.sort_unstable_by_key(|(r, rc, _vc)| (-*rc, r.clone()));
+        let mut rider_strings: Vec<String> = rider_and_ride_and_vehicle_count.iter()
+            .map(|(rider_name, ride_count, vehicle_count)| {
+                let ride_text = if *ride_count == 1 {
+                    "one ride".to_owned()
+                } else {
+                    format!("{} rides", ride_count)
+                };
+                let vehicle_text = if *vehicle_count == 1 {
+                    "one vehicle".to_owned()
+                } else {
+                    format!("{} vehicles", vehicle_count)
+                };
+                let uniqueness_percentage = (*vehicle_count as f64) * 100.0 / (*ride_count as f64);
+
+                format!("{} ({} in {}; {:.2}% unique)", rider_name, ride_text, vehicle_text, uniqueness_percentage)
+            })
+            .collect();
+        let prefix = if rider_strings.len() < 6 {
+            "Top riders:\n"
+        } else {
+            rider_strings.drain(5..);
+            "Top 5 riders:\n"
+        };
+        let riders_string = rider_strings.join("\n");
+
+        let response_string = if riders_string.len() == 0 {
+            "No top riders.".to_owned()
+        } else {
+            format!("{}{}", prefix, riders_string)
+        };
+
+        send_channel_message!(
+            interface,
+            &channel_message.channel.name,
+            &response_string,
+        ).await;
+    }
+}
